f4e78f2cf7e9735242637cca2ebda22aa7916aea
Vibha Singhal <vibha.singhal@gmail.com>
2013-07-07 17:16:35 +0000

    Modify web.js

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+var infile = "index.html";
+var line = fs.readFileSync(infile).toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(line);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
